6e45e610563f8090580ce5517438830d199c09e4
wangsteven3 <wangsteven3@gmail.com>
2020-12-01 00:05:09 -0500

    Updated pre alarm set

---
+++ app/src/main/java/com/example/sleeptest/TimePicker.java
@@ -0,0 +1,25 @@
+package com.example.sleeptest;
+
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
+
+import java.util.Calendar;
+
+public class TimePicker extends DialogFragment {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        //get real time from current device
+        Calendar current = Calendar.getInstance();
+        int hr = current.get(Calendar.HOUR_OF_DAY);
+        int min = current.get(Calendar.MINUTE);
+
+        return new TimePickerDialog(getActivity(),(TimePickerDialog.OnTimeSetListener) getActivity(), hr, min, android.text.format.DateFormat.is24HourFormat(getActivity()));
+    }
+}
--- app/src/main/java/com/example/sleeptest/alarm.java
+++ app/src/main/java/com/example/sleeptest/alarm.java
@@ -1,11 +1,15 @@
 package com.example.sleeptest;
 
-import android.app.Activity;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
-public class alarm extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+
+public class alarm extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) { //alarm xml
@@ -23,8 +27,19 @@ public class alarm extends Activity {
         }
     }
 
+//    Button button = (Button) findViewById(R.id.button);
+//    button.setOnClickListener(new View.OnClickListener())  {
+               // @Override
+                public void alarmSet(View v) {
+                    DialogFragment timePick = new TimePicker();
+                    timePick.show(getSupportFragmentManager(), "Time Picker");
 
+                }
+//        }
 
-
-
+    @Override
+    public void onTimeSet(android.widget.TimePicker view, int hourOfDay, int minute) {
+        TextView textView = (TextView)findViewById(R.id.textView);
+        textView.setText("Hour: " + hourOfDay + " Minute: " + minute);
+    }
 }
